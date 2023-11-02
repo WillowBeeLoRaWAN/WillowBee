8f6d77eedcc2aaf3be0551ec9fd2439b10571298
WillowBee <tech@bipom.com>
2023-11-03 00:12:37 +0300

    timer_1sec added

---
+++ WBL1/examples/timer_1sec/MDK-ARM/RTE/_timer_1sec/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'timer_1sec' 
+ * Target:  'timer_1sec' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32wlxx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
